87bed17e313b59b972849b7a38ec9df1a57fd7d5
Douglas Collioni <douglascollioni@bluebridgetech.com>
2021-03-07 18:26:57 +0000

    Modify mongo connection url to config

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,7 @@ process.env.PORT = process.env.PORT || '8080'
 const config = {
   nodeEnv: process.env.NODE_ENV,
   port: parseInt(process.env.PORT, 10),
+  mongoUri: process.env.MONGODB_URI
 }
 
 export default config
--- src/data/connections/mongodb.connection.ts
+++ src/data/connections/mongodb.connection.ts
@@ -1,12 +1,13 @@
 import { Collection, Db, MongoClient } from 'mongodb'
+import config from './../../config'
 
-const url = process.env.MONGODB_URL
+const uri = config.mongoUri
 const dbName = 'udacity-books'
 
 let _client: MongoClient
 const client = async (): Promise<Db> => {
   if (!_client) {
-    _client = await MongoClient.connect(url, { useUnifiedTopology: true })
+    _client = await MongoClient.connect(uri, { useUnifiedTopology: true })
   }
 
   return _client.db(dbName)
