24943505be87e1eb0a6942c5e6c8e339ea490152
Douglas Collioni <douglascollioni@bluebridgetech.com>
2021-03-13 18:20:48 +0000

    Update ingress

--- kube/deploy.sh
+++ kube/deploy.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+# first deploy
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.44.0/deploy/static/provider/aws/deploy.yaml
+# kubectl apply -f deploy-tls-termination.yaml
+
 kubectl apply -f ./kube
 ./kube/deploy-secret.sh
 ./kube/deploy-autoscale.sh
